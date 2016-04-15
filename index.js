f4bdd1d9a772330f73af3b2302371a3a2fc50104
Tonia B <toniabeglari@gmail.com>
2016-04-14 18:00:00 -0700

    setup trump

---
+++ index.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var osc = require('node-osc');
+var oscServer = new osc.Server(3333, '127.0.0.1');
+
+app.use(express.static(__dirname + '/public'));
+
+/*oscServer.on("message", function (msg, rinfo) {
+  console.log(msg);
+});*/
+
+io.on('connection', function(socket){
+
+   /*var x = 0;
+   setInterval (function () {
+     x += 0.01;
+     // console.log(Math.sin(x)+1);
+     //socket.emit('dial', { 'dial': Math.sin(x)+1 });
+   }, 1000);*/
+
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  oscServer.on("message", function (msg, rinfo) {
+    console.log("TouchOSC message:");
+    console.log(msg);
+
+    if (msg[0].startsWith('/1/push')) {
+      var val = msg[0].substr(-1);
+      socket.emit('griddata', { 'button': val });
+    }
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
